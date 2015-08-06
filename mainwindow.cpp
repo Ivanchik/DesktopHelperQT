f90129a63fa899649fed7e7f95092994516b1c45
nastya13 <nastya94-94.08@mail.ru>
2015-08-06 13:09:33 +0300

    creating class for parsing apis

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    wd = new WeatherData("http://export.yandex.ru/weather-ng/forecasts/27595.xml");
+
+    ui->temperatureValueLabel->setText(static_cast<QString>(wd->GetTemperature()));
+    ui->pressureValueLabel->setText(static_cast<QString>(wd->GetPressure()));
+    ui->windValueLabel->setText(static_cast<QString>(wd->GetWindSpeed()));
 }
 
 MainWindow::~MainWindow()
